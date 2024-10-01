c298391335f589d513600358a76e592c380b49d0
Hosea <karanja.mungai23@gmail.com>
2024-10-01 23:24:24 +0300

    modifies

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const removeMain = document.getElementById("main");
+removeMain.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "Hosea is the champion!"
